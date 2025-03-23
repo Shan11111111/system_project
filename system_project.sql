bcebb012f0527813b11c7b88d1c332cb6703afc3
Shan11111111 <09shan2005@gmail.com>
2025-03-23 19:15:17 +0800

    資料庫 #86ervbhbd

---
+++ system_project.sql
@@ -0,0 +1,256 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2025-03-23 11:56:31
+-- 伺服器版本： 10.4.32-MariaDB
+-- PHP 版本： 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `system_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `advice`
+--
+
+CREATE TABLE `advice` (
+  `advice_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `advice_title` varchar(20) DEFAULT NULL,
+  `advice_content` text DEFAULT NULL,
+  `agree` int(11) DEFAULT 0,
+  `category` varchar(20) DEFAULT NULL,
+  `state` varchar(20) DEFAULT '未處理',
+  `announce_date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 傾印資料表的資料 `advice`
+--
+
+INSERT INTO `advice` (`advice_id`, `user_id`, `advice_title`, `advice_content`, `agree`, `category`, `state`, `announce_date`) VALUES
+(1, 412402001, '泳池不乾淨', '提議學生有薪水的打掃泳池', 0, ' 環境', '未處理', '2025-03-23'),
+(2, 412402001, '泳池超級臭', '學校趕快清理', 0, '環境', '未處理', '2025-03-23');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `agree_record`
+--
+
+CREATE TABLE `agree_record` (
+  `agree_record_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `advice_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `comments`
+--
+
+CREATE TABLE `comments` (
+  `comment_id` int(11) NOT NULL,
+  `advice_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `comment_content` text DEFAULT NULL,
+  `comment_time` date DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 傾印資料表的資料 `comments`
+--
+
+INSERT INTO `comments` (`comment_id`, `advice_id`, `user_id`, `comment_content`, `comment_time`) VALUES
+(1, 2, 412402001, '沒有人想去刷泳池...', '2025-03-23');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `funding`
+--
+
+CREATE TABLE `funding` (
+  `funding_id` int(11) NOT NULL,
+  `advice_id` int(11) DEFAULT NULL,
+  `money` int(11) DEFAULT 0,
+  `target` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `funding_people`
+--
+
+CREATE TABLE `funding_people` (
+  `funding_people_id` int(11) NOT NULL,
+  `people_name` varchar(20) DEFAULT NULL,
+  `donate_money` int(11) DEFAULT NULL,
+  `funding_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `level` varchar(20) NOT NULL,
+  `email` text DEFAULT NULL,
+  `department` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='用戶表';
+
+--
+-- 傾印資料表的資料 `users`
+--
+
+INSERT INTO `users` (`user_id`, `name`, `level`, `email`, `department`) VALUES
+(412402001, '王小明', 'student', 'example@gmail.com', '資訊管理學系');
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `advice`
+--
+ALTER TABLE `advice`
+  ADD PRIMARY KEY (`advice_id`),
+  ADD KEY `fk_user_id` (`user_id`);
+
+--
+-- 資料表索引 `agree_record`
+--
+ALTER TABLE `agree_record`
+  ADD PRIMARY KEY (`agree_record_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `advice_id` (`advice_id`);
+
+--
+-- 資料表索引 `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`comment_id`),
+  ADD KEY `advice_id` (`advice_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- 資料表索引 `funding`
+--
+ALTER TABLE `funding`
+  ADD PRIMARY KEY (`funding_id`),
+  ADD KEY `advice_id` (`advice_id`);
+
+--
+-- 資料表索引 `funding_people`
+--
+ALTER TABLE `funding_people`
+  ADD PRIMARY KEY (`funding_people_id`),
+  ADD KEY `funding_id` (`funding_id`);
+
+--
+-- 資料表索引 `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `advice`
+--
+ALTER TABLE `advice`
+  MODIFY `advice_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `agree_record`
+--
+ALTER TABLE `agree_record`
+  MODIFY `agree_record_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `funding`
+--
+ALTER TABLE `funding`
+  MODIFY `funding_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `funding_people`
+--
+ALTER TABLE `funding_people`
+  MODIFY `funding_people_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=412402002;
+
+--
+-- 已傾印資料表的限制式
+--
+
+--
+-- 資料表的限制式 `advice`
+--
+ALTER TABLE `advice`
+  ADD CONSTRAINT `advice_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+
+--
+-- 資料表的限制式 `agree_record`
+--
+ALTER TABLE `agree_record`
+  ADD CONSTRAINT `agree_record_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `agree_record_ibfk_2` FOREIGN KEY (`advice_id`) REFERENCES `advice` (`advice_id`) ON DELETE CASCADE;
+
+--
+-- 資料表的限制式 `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`advice_id`) REFERENCES `advice` (`advice_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+
+--
+-- 資料表的限制式 `funding`
+--
+ALTER TABLE `funding`
+  ADD CONSTRAINT `funding_ibfk_1` FOREIGN KEY (`advice_id`) REFERENCES `advice` (`advice_id`) ON DELETE CASCADE;
+
+--
+-- 資料表的限制式 `funding_people`
+--
+ALTER TABLE `funding_people`
+  ADD CONSTRAINT `funding_people_ibfk_1` FOREIGN KEY (`funding_id`) REFERENCES `funding` (`funding_id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
