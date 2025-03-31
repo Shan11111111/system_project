89178721a49c3d09daec6e3c521f67b617a9b493
Shan11111111 <09shan2005@gmail.com>
2025-04-01 00:56:34 +0800

    session nav

--- advice_search.php
+++ advice_search.php
@@ -21,6 +21,9 @@
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 
+    <!-- 引入 SweetAlert2 :美觀彈出未登入警告圖示-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 </head>
 
@@ -40,7 +43,36 @@
                 <div class="dropdown">
                     <button class="dropbtn">建言</button>
                     <div class="dropdown-content">
-                        <a href="submitadvice.php">發布建言</a>
+                        <?php if (isset($_SESSION['user_id'])) { ?>
+                            <a href="submitadvice.php">提交建言</a>
+                        <?php } else { ?>
+                            <a href="javascript:void(0);" onclick="showLoginAlert()">提交建言</a>
+                            <script>
+                                function showLoginAlert() {
+
+                                    Swal.fire({
+                                        icon: 'warning', // 圖示類型
+                                        title: '請先登入',
+                                        text: '發布建言為學生與教職人員專屬功能！',
+                                        confirmButtonText: '確定',
+                                        confirmButtonColor: '#3085d6',
+                                        focusConfirm: false, // 禁用自動聚焦
+                                        didOpen: () => {
+                                            // 禁用滾動
+                                            document.body.style.overflow = 'hidden';
+
+                                        },
+                                        didClose: () => {
+                                            // 恢復滾動
+                                            document.body.style.overflow = '';
+                                            // 恢復滾動位置
+
+                                        }
+                                    });
+                                }
+                            </script>
+                        <?php } ?>
+
                         <a href="advice_search.php">最新建言</a><!--之後要設(不知道是前端還後端)-->
                         <a href="advice_search.php">熱門建言</a>
                     </div>
@@ -55,8 +87,24 @@
             </div>
 
             <div class="nav-right desktop-menu">
-                <a href="login.php" class="nav-item">登入</a>
-                <a href="register.php" class="nav-item">註冊</a>
+                <?php if (isset($_SESSION['user_id'])) { ?>
+                    <a class="nav-item"><?php echo $_SESSION['user_id'] ?>會員專區</a>
+                    <a href="javascript:void(0);" class="nav-item" id="logout-link">登出</a>
+                    <script>
+                        document.getElementById('logout-link').addEventListener('click', function () {
+                            // 彈出確認視窗
+                            const confirmLogout = confirm("確定要登出嗎？");
+                            if (confirmLogout) {
+                                // 如果用戶選擇確定，導向登出頁面
+                                window.location.href = "logout.php";
+                            }
+                            // 如果用戶選擇取消，什麼都不做
+                        });
+                    </script>
+                <?php } else { ?>
+                    <a href="login.php" class="nav-item">登入</a>
+                    <a href="register.php" class="nav-item">註冊</a>
+                <?php } ?>
             </div>
         </div>
 
@@ -65,9 +113,36 @@
             <div class="dropdown">
                 <button class="dropbtn">建言</button>
                 <div class="dropdown-content">
-                    <a href="submitadvice.php">發布建言</a>
-                    <a href="">最新建言</a><!--之後要設(不知道是前端還後端)-->
-                    <a href="#">熱門建言</a>
+                    <?php if (isset($_SESSION['user_id'])) { ?>
+                        <a href="submitadvice.php">提交建言</a>
+                    <?php } else { ?>
+                        <a href="javascript:void(0);" onclick="showLoginAlert()">提交建言</a>
+                        <script>
+                            function showLoginAlert() {
+                                Swal.fire({
+                                    icon: 'warning', // 圖示類型
+                                    title: '請先登入',
+                                    text: '發布建言為學生與教職人員專屬功能！',
+                                    confirmButtonText: '確定',
+                                    confirmButtonColor: '#3085d6',
+                                    focusConfirm: false, // 禁用自動聚焦
+                                    didOpen: () => {
+                                        // 禁用滾動
+                                        document.body.style.overflow = 'hidden';
+                                    },
+                                    didClose: () => {
+                                        // 恢復滾動
+                                        document.body.style.overflow = '';
+                                        // 恢復滾動位置
+                                        window.scrollTo(0, scrollTop);
+                                    }
+                                });
+                            }
+                        </script>
+                    <?php } ?>
+
+                    <a href="advice_search.php">最新建言</a>
+                    <a href="advice_search.php">熱門建言</a>
                 </div>
             </div>
             <div class="dropdown">
@@ -77,8 +152,26 @@
                     <a href="#">成功案例</a>
                 </div>
             </div>
-            <a href="login.php" class="nav-item">登入</a>
-            <a href="register.php" class="nav-item">註冊</a>
+
+            <?php if (isset($_SESSION['user_id'])) { ?>
+                <a class="nav-item"><?php echo $_SESSION['user_id'] ?>會員專區</a>
+                <a class="nav-item" id="logout-link-mobile">登出</a>
+                <script>
+                    document.getElementById('logout-link-mobile').addEventListener('click', function () {
+                        // 彈出確認視窗
+                        const confirmLogout = confirm("確定要登出嗎？");
+                        if (confirmLogout) {
+                            // 如果用戶選擇確定，導向登出頁面
+                            window.location.href = "logout.php";
+                        }
+                        // 如果用戶選擇取消，什麼都不做
+                    });
+                </script>
+            <?php } else { ?>
+                <a href="login.php" class="nav-item">登入</a>
+                <a href="register.php" class="nav-item">註冊</a>
+            <?php } ?>
+
         </div>
     </nav>
 
