ef618154ed85a35a9ebdcdc0db5101d1ddd8803b
Shan11111111 <09shan2005@gmail.com>
2025-04-25 18:09:14 +0800

    募資截止60天後

--- manager/advice_manager.php
+++ manager/advice_manager.php
@@ -224,7 +224,9 @@ $total_pages = ceil($total_rows / $limit);
         <a href="../homepage.php">孵仁首頁</a>
         <a href="advice_manager.php">建言管理</a>
         <a href="assign_office.php">達標建言分配處所</a>
-        <a href="review_proposals.php">審核</a>
+        <a href="review_proposals.php">募資專案審核</a>
+        <a href="project_manager.php">募資管理</a>
+        <a href="review_extension_requests.php">延後募資申請審核</a>
         <a href="people_manager.php">人員處理</a>
         <a href="#">數據分析</a>
     </div>
