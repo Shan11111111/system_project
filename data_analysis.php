0c5d44701cd31b07d48805736be7b19ec58d5b2f
name <thomas92520@gmail.com>
2025-05-13 15:00:27 +0800

    debug

--- data_analysis.php
+++ data_analysis.php
@@ -4,7 +4,6 @@ $host = 'localhost';
 $dbname = 'system_project';
 $username = 'root';
 $password = '';
-
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
