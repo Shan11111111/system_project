72d93b4498977642a055a9ac307d1281e691f602
Annex0604 <412402012@m365.fju.edu.tw>
2025-05-17 08:49:14 +0800

    announcement表修改

--- system_project.sql
+++ system_project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2025-05-14 13:09:46
+-- 產生時間： 2025-05-17 02:48:34
 -- 伺服器版本： 10.4.32-MariaDB
 -- PHP 版本： 8.0.30
 
@@ -297,7 +297,7 @@ CREATE TABLE `announcement` (
   `content` varchar(255) NOT NULL,
   `category` enum('建言','募資','系統') NOT NULL,
   `update_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `file_path` varchar(255) NOT NULL,
+  `file_path` varchar(255) DEFAULT NULL,
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -884,7 +884,7 @@ ALTER TABLE `agree_record`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `announcement`
 --
 ALTER TABLE `announcement`
-  MODIFY `announcement_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `announcement_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `collection`
